2a9bdb39d50eeacba85db6499bb08a18acb10bbf
Qicwken <qicwken@gmail.com>
2025-12-27 09:01:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,3 +39,57 @@ cat << 'EOF' > package/base-files/files/etc/banner
    \  RI    \  /    \s \r
     \________\/    -------------------------------------------
 EOF
+
+# 写入 /etc/config/wireless
+mkdir -p package/base-files/files/etc/config
+
+cat << 'EOF' > package/base-files/files/etc/config/wireless
+config wifi-device 'radio0'
+	option type 'mac80211'
+	option path 'platform/soc/18000000.wifi'
+	option channel '1'
+	option band '2g'
+	option htmode 'HE20'
+	option disabled '0'
+	option country 'CN'
+	option cell_density '3'
+	option mu_beamformer '1'
+	option vendor_vht '1'
+
+config wifi-iface 'default_radio0'
+	option device 'radio0'
+	option network 'lan'
+	option mode 'ap'
+	option ssid 'rd03_minet_12ac'
+	option encryption 'none'
+	option multicast_to_unicast '1'
+	option ieee80211k '1'
+	option ieee80211v '1'
+	option wnm_sleep_mode '0'
+	option time_advertisement '0'
+	option wpa_disable_eapol_key_retries '1'
+
+config wifi-device 'radio1'
+	option type 'mac80211'
+	option path 'platform/soc/18000000.wifi+1'
+	option channel '36'
+	option band '5g'
+	option htmode 'HE160'
+	option disabled '0'
+	option country 'CN'
+	option cell_density '3'
+	option mu_beamformer '1'
+
+config wifi-iface 'default_radio1'
+	option device 'radio1'
+	option network 'lan'
+	option mode 'ap'
+	option ssid 'rd03_minet_12ac_5G'
+	option encryption 'none'
+	option multicast_to_unicast '1'
+	option ieee80211k '1'
+	option ieee80211v '1'
+	option wnm_sleep_mode '0'
+	option time_advertisement '0'
+	option wpa_disable_eapol_key_retries '1'
+EOF
