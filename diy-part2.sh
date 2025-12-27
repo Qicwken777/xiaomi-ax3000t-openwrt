cbe53b34b1cf8fc523d9b76a4538c8cd432a2421
Qicwken <qicwken@gmail.com>
2025-12-27 08:16:05 +0800

    Change default IP from 192.168.1.1 to 192.168.12.1

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/luci2/bin/config_generate
+
+# 修改默认 IP
+sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
